b6a4efe3f7511b6e750ad8c94554bdc54101a87a
Amit Kumar Chaudhary <113765935+chaudharytechy@users.noreply.github.com>
2024-05-28 11:58:09 +0530

    Update AboutController.jsaws

--- Controller/AdminController/FrontController/AboutController.js
+++ Controller/AdminController/FrontController/AboutController.js
@@ -7,7 +7,7 @@ const cloudinary = require('cloudinary').v2;
 class aboutController {
 
     static about = async (req, res) => {
-        res.send("about page listen")
+        res.send("about page listen aws listen")
     }
 
     static aboutInsert = async (req, res) => {
